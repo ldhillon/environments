8789152be0eee231e5c439cc9d52aa19386589e4
ldhillon <ldhillon@gmail.com>
2016-05-08 04:34:17 +0000

    add mco host 2

--- site.pp
+++ site.pp
@@ -3,10 +3,10 @@ node default {
 }
 
 node puppetmaster.davita.corp {
- class { '::mcollective':
-   middleware   => true,
-   client       => true,
-   middleware_hosts => [ 'puppetmaster.davita.corp' ],
+  class { '::mcollective':
+    middleware   => true,
+    client       => true,
+    middleware_hosts => [ 'puppetmaster.davita.corp' ],
    }
 }
 
