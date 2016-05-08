855f77088d41acaf3854264128aec33e846c6417
ldhillon <ldhillon@gmail.com>
2016-05-08 04:24:23 +0000

    add mco

--- site.pp
+++ site.pp
@@ -2,3 +2,11 @@ node default {
    include helloworld
 }
 
+node puppetmaster {
+ class { '::mcollective':
+   middleware   => true,
+   client       => true,
+   middleware_hosts => [ 'puppetmaster.davita.corp' ],
+   }
+}
+
